d7710fd2e7b049cdbbafe5f1834df3b98248f563
Douglas <dglandre@gmail.com>
2022-10-08 18:06:57 -0300

    Configurando conexão do ServiceBus com SBSender

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ServiceBusDemo
+
+## Configurações
+###SBReceiver - Console App
+netcoreapp3.1
+Microsoft.Azure.ServiceBus" Version="5.0.0
+
+###SBSender
+netcoreapp3.1 - Blazor App
+Microsoft.Azure.ServiceBus" Version="5.0.0
+
+###SBShared
+netstandard2.0 - Class Library
+System.ComponentModel.Annotations" Version="5.0.0"
--- SBSender/Startup.cs
+++ SBSender/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SBSender.Data;
+using SBSender.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +30,7 @@ namespace SBSender
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddTransient<IQueueService, QueueService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
