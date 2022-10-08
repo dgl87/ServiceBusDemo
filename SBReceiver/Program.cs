25ea64a439d9d38a6f64fef6d8270beca77a023f
Douglas <dglandre@gmail.com>
2022-10-08 19:14:04 -0300

    Finalizado SBReceiver

--- SBReceiver/Program.cs
+++ SBReceiver/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using Microsoft.Azure.ServiceBus;
+using SBShared.Models;
+using System;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SBReceiver
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        const string connectionString = "Endpoint=sb://dglzservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Gs4UvPsCv8jBfGTB1NSMvhD4LkppZB8OpIhGsaVvWLE=";
+        const string queueName = "personqueue";
+        static IQueueClient queueClient;
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            queueClient = new QueueClient(connectionString, queueName);
+
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            queueClient.RegisterMessageHandler(ProcessMessageAsync, messageHandlerOptions);
+
+            Console.ReadLine();
+
+            await queueClient.CloseAsync();
+        }
+
+        private static async Task ProcessMessageAsync(Message message, CancellationToken token)
+        {
+            var jsonString = Encoding.UTF8.GetString(message.Body);
+            PersonModel person = JsonSerializer.Deserialize<PersonModel>(jsonString);
+            Console.WriteLine($"Person Received: { person.FirstName } { person.LastName }");
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs arg)
+        {
+            Console.WriteLine($"Message handler exception: { arg.Exception }");
+            return Task.CompletedTask;
         }
     }
 }
